9d09e5122fd0f4916b2ec759d3e46ee5fc8f3048
Lohitha Yerramsetty <lohitha.yerramsetty@gmail.com>
2020-09-03 13:22:21 +0200

    SQL Lab 6 Done

---
+++ WebApplication/WebApiEntityFramework/Models/ApiContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApiEntityFramework.Models
+{
+    public partial class ApiContext : DbContext
+    {
+        public ApiContext()
+        {
+        }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.Property(e => e.Id).HasMaxLength(128);
+
+                entity.Property(e => e.Name).HasMaxLength(30);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplication/WebApiEntityFramework/Models/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiEntityFramework.Models
+{
+    public partial class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public Products()
+        {
+
+        }
+        public Products(int id, string name, string description, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+
+}
