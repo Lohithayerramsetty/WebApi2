4c857ee4a9c37da6900a520be1a9eb3f30ae532c
Lohitha Yerramsetty <lohitha.yerramsetty@gmail.com>
2020-09-03 14:20:45 +0200

    Minor Changes

--- WebApplication/WebApiEntityFramework/Startup.cs
+++ WebApplication/WebApiEntityFramework/Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApiEntityFramework.Models;
 
 namespace WebApiEntityFramework
 {
@@ -21,6 +24,11 @@ namespace WebApiEntityFramework
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApiContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
+            services.AddMvc(options => options.EnableEndpointRouting = false).
+                SetCompatibilityVersion(CompatibilityVersion.Latest).AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
