382fdc05ae4f7d7d23f1cbf688abbc9eb09a25b4
Lohitha Yerramsetty <lohitha.yerramsetty@gmail.com>
2020-09-04 14:23:55 +0200

    Lab-8 Done

---
+++ WebApplication/BlazorWithApi/Data/BlazorWithApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWithApi.Entities;
+
+namespace BlazorWithApi.Data
+{
+    public class BlazorWithApiContext : DbContext
+    {
+        public BlazorWithApiContext (DbContextOptions<BlazorWithApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWithApi.Entities.Customer> Customer { get; set; }
+    }
+}
